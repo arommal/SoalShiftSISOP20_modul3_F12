2aeeb40cf763add13b92b3d5cb837bd91f63d52b
arommal <rosalammora@gmail.com>
2020-04-08 19:54:18 +0700

    Add existing files

--- soal1/soal1_pokezone.c
+++ soal1/soal1_pokezone.c
@@ -1,56 +1,192 @@
-#include<stdio.h>
-#include<string.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
 
-// int encrate = 0;
-int escrate = 0;
-int caprate = 0;
-int pokedol = 0;
-int rarity = 0;
-char nama[10];
-void randompokemon(){
-    int x =rand()%100;
-    if (x<=79){
-        rarity = 1;
-        escrate = 5;
-        caprate = 70;
-        pokedol = 80;
-        int y = rand()%5;
-        memset (nama,0,sizeof(nama));
-        if (y==0) strcpy (nama, "Bulbasaur");
-        else if (y==1) strcpy (nama, "Charmender");
-        else if (y==2) strcpy (nama, "Squirtle");
-        else if (y==3) strcpy (nama, "Rattata");
-        else if (y==4) strcpy (nama, "Caterpie");
+typedef struct WildPokemon_s{
+	int isLocked;
+	char name[20];
+	int type;
+	int price;
+	int capRate;
+	int escapeRate;
+}WildPokemon_t;
+
+typedef struct Shop_s{
+	int isLocked;
+	int lullabyPowder;
+	int pokeball;
+	int berry;
+}Shop_t;
+
+typedef struct GameData_s{
+	int isRunning;
+	WildPokemon_t * sharedPokemon;
+	Shop_t * sharedShop;
+
+}GameData_t;
+
+char arr[20];
+void * randomizePokemonThread(void * param);
+void * shopThread(void * param);
+
+
+time_t t;
+srand ((unsigned) timr(&t));
+
+int main(){	
+	//shared memory
+	key_t key = 1111;
+	WildPokemon_t * sharedPokemon;
+	int shmid =  shmget(key, sizeof(WildPokemon_t), IPC_CREAT | 0666);
+	sharedPokemon = shmat(shmid, NULL, 0);
+	memset(sharedPokemon, 0, sizeof(sharedPokemon));
+
+	key_t key2 = 2222;
+	Shop_t * sharedShop;
+	int shmid2 =  shmget(key2, sizeof(Shop_t), IPC_CREAT | 0666);
+	sharedShop = shmat(shmid2, NULL, 0);
+	memset(sharedShop, 0, sizeof(sharedShop));
+
+	GameData_t * gameData = malloc(sizeof(GameData_t));
+	memset(gameData, 0, sizeof(GameData_t));
+    gameData->isRunning = 1;
+	gameData->sharedShop = sharedShop;
+    gameData->sharedPokemon = sharedPokemon;
+
+    //thread
+    pthread_t thread1, thread2;
+ 	if(pthread_create(&thread1, NULL, &randomizePokemonThread, (void*)gameData ) < 0){
+		printf("can't create thread\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if(pthread_create(&thread2, NULL, &shopThread, (void*)gameData ) < 0){
+		printf("can't create thread\n");
+		exit(EXIT_FAILURE);
+	}  
+    
+    printf("Pokezone\n1. Shutdown game\nChoice : ");
+    scanf("%s", arr);
+    if (strcmp(arr, "shutdown")==0){
+        pid_t id = fork();
+		if(!id){
+			FILE * pidProcess = popen("pidof soal1_traizone", "r");
+			int pida = 0;
+			while(fscanf(pidProcess, "%d", &pida) != EOF){
+				id = fork();
+				if(!id){
+					char pid[10];
+					snprintf(pid, sizeof(pid), "%d", pida);
+					char * argv[] = {"kill", "-9", pid, NULL};
+					execv("/bin/kill", argv);
+				}
+			}
+			char pid[10];
+			snprintf(pid, sizeof(pid), "%d", ppp);
+			char * argv[] = {"kill", "-9", pid, NULL};
+			execv("/bin/kill", argv);
+		}
     }
-    else if (x<=94){
-        rarity = 2;
-        escrate = 10;
-        caprate = 50;
-        pokedol = 100;
-        int y = rand()%5;
-        memset (nama,0,sizeof(nama));
-        if (y==0) strcpy (nama, "Pikachu");
-        else if (y==1) strcpy (nama, "Eevee");
-        else if (y==2) strcpy (nama, "Jigglypuff");
-        else if (y==3) strcpy (nama, "Snorlax");
-        else if (y==4) strcpy (nama, "Dragonite");
+
+	pthread_join(pokemonThread, NULL);
+	pthread_join(shop, NULL);
+
+	shmdt(sharedPokemon);
+    shmctl(shmid, IPC_RMID, NULL);
+
+	shmdt(sharedShop);
+    shmctl(shmid2, IPC_RMID, NULL);
+	return 0;
+}
+
+void * randomizePokemonThread(void * param){
+	GameData_t * gameData = (GameData_t *)param;
+	WildPokemon_t * sharedPokemon = gameData->sharedPokemon;
+
+    while(gameData->isRunning){
+		if (sharedPokemon->isLocked){
+			continue;
+		}
+        int x= rand()%100;
+        if (x<=79){
+            sharedPokemon->type = 1;
+            sharedPokemon->escapeRate = 5;
+            sharedPokemon->capRate = 70;
+            sharedPokemon->price = 80;
+            int y = rand()%5;
+            memset (sharedPokemon->name,0,sizeof(sharedPokemon->name));
+            if (y==0) strcpy (sharedPokemon->name, "Bulbasaur");
+            else if (y==1) strcpy (sharedPokemon->name, "Charmender");
+            else if (y==2) strcpy (sharedPokemon->name, "Squirtle");
+            else if (y==3) strcpy (sharedPokemon->name, "Rattata");
+            else if (y==4) strcpy (sharedPokemon->name, "Caterpie");
+        }
+        else if (x<=94){
+            sharedPokemon->type = 2;
+            sharedPokemon->escapeRate = 10;
+            sharedPokemon->capRate = 70;
+            sharedPokemon->price = 100;
+            int y = rand()%5;
+            memset (sharedPokemon->name,0,sizeof(sharedPokemon->name));
+            if (y==0) strcpy (sharedPokemon->name, "Pikachu");
+            else if (y==1) strcpy (sharedPokemon->name, "Eevee");
+            else if (y==2) strcpy (sharedPokemon->name, "Jigglypuff");
+            else if (y==3) strcpy (sharedPokemon->name, "Snorlax");
+            else if (y==4) strcpy (sharedPokemon->name, "Dragonite");
+        }
+        else if (x<=99){
+            sharedPokemon->type = 3;
+            sharedPokemon->escapeRate = 20;
+            sharedPokemon->capRate = 50;
+            sharedPokemon->price = 200;
+            int y = rand()%5;
+            memset (sharedPokemon->name,0,sizeof(sharedPokemon->name));
+            if (y==0) strcpy (sharedPokemon->name, "Mew");
+            else if (y==1) strcpy (sharedPokemon->name, "Mewtwo");
+            else if (y==2) strcpy (sharedPokemon->name, "Moltres");
+            else if (y==3) strcpy (sharedPokemon->name, "Zapdos");
+            else if (y==4) strcpy (sharedPokemon->name, "Articuno");
+        }
+        int z=rand()%8000;
+        if (z==0){
+            sharedPokemon->escapeRate += 5;
+            sharedPokemon->capRate -= 20;
+            sharedPokemon->price += 5000;
+        }
     }
-    else if (x<=99){
-        rarity = 3;
-        escrate = 20;
-        caprate = 50;
-        pokedol = 200;
-        int y = rand()%5;
-        memset (nama,0,sizeof(nama));
-        if (y==0) strcpy (nama, "Mew");
-        else if (y==1) strcpy (nama, "Mewtwo");
-        else if (y==2) strcpy (nama, "Moltres");
-        else if (y==3) strcpy (nama, "Zapdos");
-        else if (y==4) strcpy (nama, "Articuno");
-    }
+
+    // int isLocked;
+	// char name[20];
+	// int type;
+	// int price;
+	// int capRate;
+	// int escapeRate;
+}
+
+void * shopThread(void * param){
+	GameData_t * gameData = (GameData_t *)param;
+	gameData->sharedShop->lullabyPowder = 100;
+	gameData->sharedShop->berry = 100;
+	gameData->sharedShop->pokeball = 100;
+	// fflush(stdout);
+	while(gameData->isRunning){
+		sleep(10);
+		gameData->sharedShop->berry += 10;
+		if(gameData->sharedShop->berry > 200){
+			gameData->sharedShop->berry = 200;
+		}
+		gameData->sharedShop->pokeball += 10;
+		if(gameData->sharedShop->pokeball > 200){
+			gameData->sharedShop->pokeball = 200;
+		}
+		gameData->sharedShop->lullabyPowder += 10;
+		if(gameData->sharedShop->lullabyPowder > 200){
+			gameData->sharedShop->lullabyPowder = 200;
+		}
+		// fflush(stdout);
+	}
 }
--- soal1/soal1_traizone.c
+++ soal1/soal1_traizone.c
@@ -1,72 +1,304 @@
-#include<stdio.h>
-#include<string.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-typedef struct{
-    char[10] nama;
+typedef struct WildPokemon_s{
+	int isLocked;
+	char name[20];
+	int type;
+	int price;
+	int capRate;
+	int escapeRate;
+}WildPokemon_t;
 
-}pokemon;
-typedef struct{
-    int caripokemon=0;
-} data;
-char pokemonnow[7][10];
-int jmlpokemon=0;
+typedef struct Shop_s{
+	int isLocked;
+	int lullabyPowder;
+	int pokeball;
+	int berry;
+}Shop_t;
 
-int state=0;
-data * game;
-int shmid = shmget(key, sizeof(data), IPC_CREAT | 0666);
-game = shmat(shmid, NULL, 0);
+typedef struct Pokemon_s{
+	int released;
+	char name[20];
+	int type;
+	int ap;
+	int price;
+}Pokemon_t;
+
+typedef struct GameData_s{
+	int isRunning;
+	int jumlahLullabyActive;
+	WildPokemon_t * sharedPokemon;
+	Shop_t * sharedShop;
+}GameData_t;
+
+Pokemon_t pokemon[7];
+int pokecount = 0;
+
+int pokeBallc=10;
+int lullabyPowderc=0;
+int berryc=0;
+int pokedollar=0;
+
+int screen;
+int cariPokemon=0;
+
+void * cariPokemonThread(void * param);
+void * wildpokemonThread(void * param);
+void * lullabyThread(void * param);
+void * pokemonThread(void * param);
+
+char arr[20];
 time_t t;
-
 srand ((unsigned) timr(&t));
+int *isrunning;
+int state=0;
+int paramisrunning;
 int main(){
-    // ket_t key = 1234;
-    // int shmid = shmget(key, sizeof)
-    while (1){
+    key_t key = 1111;
+	WildPokemon_t * sharedPokemon;
+	int shmid =  shmget(key, sizeof(WildPokemon_t), IPC_CREAT | 0666);
+	sharedPokemon = shmat(shmid, NULL, 0);
+
+	key_t key2 = 2222;
+	Shop_t * sharedShop;
+	int shmid2 =  shmget(key2, sizeof(Shop_t), IPC_CREAT | 0666);
+	sharedShop = shmat(shmid2, NULL, 0);
+
+    GameData_t * gameData = malloc(sizeof(GameData_t));
+	memset(gameData, 0, sizeof(GameData_t));
+	gameData->sharedShop = sharedShop;
+    gameData->sharedPokemon = sharedPokemon;
+
+    *isRunning = &gameData->isRunning;
+	*isRunning = 1;
+    while (*isRunning){
         if (state==0){
-            int menu;
-            printf ("1. Cari pokemon\n2. Pokedex\n3. Shop\n");
-            scanf ("%d", &menu);
-            if (menu == 1 ){
-                game->caripokemon = 1;
-                pthread_t thread;
-                if (pthread_create(&thread, NULL, &caripokemon,NULL)<0){
-                    printf ("cant\n");
-                    exit(EXIT_FAILURE);
+            printf ("Normal Mode\n");
+            if (caripokemon) printf("1. Berhenti cari\n2. Pokedex\n3. Shop\nChoice : ");
+            else printf("1. Cari pokemon\n2. Pokedex\n3. Shop\nChoice : ");
+            memset(arr,0,sizeof(arr));
+            scanf("%s",arr);
+            if (strcmp(arr,"cari")==0){
+                if (caripokemon) cariPokemon=0;
+                else{
+                    caripokemon = 1;
+                    pthread_t thread;
+                    if(pthread_create(&thread, NULL, &cariPokemonThread, (void *)gameData) < 0){
+                        printf("can't create thread\n");
+                        exit(EXIT_FAILURE);
+                    }
                 }
             }
-            else if (menu == 2){
-                for (int i=0;i<7;i++){
-                    printf ("%s\n", pokemonnow[i]);
-                }
-            }
-            else if (menu ==3){
-                printshop();
-            }
+            else if (strcmp(arr,"pokedex")==0) state=1;
+            else if (strcmp(arr,"shop")==0) state=2;
         }
-        else if (state==10){
-            
+        else if (state==1){
+            int i;
+            printf ("Pokedex\n");
+            if (pokecount==0) printf("Anda tidak memiliki pokemon\n");
+            else {
+                for (i=0;i<pokecount;i++) printf ("Name = %s - AP = %d\n",pokemon[i].name, pokemon[i].ap);
+            }
+
+            printf ("\nAnda memiliki %d Berry\n1. Beri berry\n2/ Lepas pokemon\n3. Kembali ke normal mode\nChoice : ");
+            memset(arr,0,sizeof(arr));
+            scanf("%s",arr);
+            if (strcmp(arr,"berry")==0){
+                if (pokecount==0) printf("Anda tidak memiliki pokemon\n");
+                else {
+                    if (berryc>=1){
+                        berryc--;
+                        for(i=0;i<pokecount;i++) pokemon[i].ap+=10;
+                        printf ("Berry telah diberikan\n");
+                    }
+                    else printf("Anda tidak memiliki berry\n");
+                }
+            }
+            else if (strcmp(arr,"lepas")==0){
+                if (pokecount==0) printf("Anda tidak memiliki pokemon\n");
+                else{
+                    int x;
+                    printf ("Masukan index pokemon yang anda ingin lepas : ");
+                    scanf("%d",&x);
+                    pokecount--;
+                    pokemon[x].released=1;
+                }
+            }
+            else if (strcmp(arr,"kembali")==0) state=0;
+        }
+        else if (state == 2){
+            printf ("Shop\n");
+            Shop_t * sharedShop = gameData->sharedShop;
+            printf("List item :\n1. Lullaby Powder - Stok : %d - Harga : 60\n",sharedShop->lullabyPowder);
+            printf("2. Pokeball - Stok : %d - Harga : 5\n",sharedShop->pokeball);
+            printf("3. Berry - Stok : %d - Harga : 15\n",sharedShop->berry);
+            printf ("Anda memiliki :\n");
+            printf ("%d Pokedollar - %d Lullaby Powder - %d Pokeball - %d Berry\n", pokedollar, lullabyPowderc, pokeBallc, berryc);
+            printf ("\n1. Belli Lullaby Powder\n2. Beli Pokeball\n3. Beli Berry\n4. Kembali ke normal mode\nChoice : ");
+            int z;
+            scanf ("%d",&z);
+            if (z==1){
+                if (pokedollar < 60) printf ("Uang tidak cukup");
+                else {
+                    if (sharedShop->lullabyPowder > 0){
+                        printf ("Lullaby powder telah dibeli\n");
+                        lullabyPowderc++;
+                        pokedollar-=60;
+                        sharedShop->lullabyPowder--;
+                    }
+                    else printf("Stok habis\n");
+                }
+            }
+            else if (z==2){
+                if (pokedollar < 5) printf ("Uang tidak cukup");
+                else {
+                    if (sharedShop->pokeball > 0){
+                        printf ("pokeball telah dibeli\n");
+                        pokeballc++;
+                        pokedollar-=5;
+                        sharedShop->pokeball--;
+                    }
+                    else printf("Stok habis\n");
+                }
+            }
+            else if (z==3){
+                if (pokedollar < 15) printf ("Uang tidak cukup");
+                else {
+                    if (sharedShop->berry > 0){
+                        printf ("pokeball telah dibeli\n");
+                        berryc++;
+                        pokedollar-=15;
+                        sharedShop->berry--;
+                    }
+                    else printf("Stok habis\n");
+                }
+            }
+            else if (z==4) state = 0;
+        }
+        else if (state == 3){
+            WildPokemon_t * wpokemon = gameData->sharedPokemon;
+	        wpokemon->isLocked = 1;
+	        pthread_t wild;
+            paramisrunning=1;
+            if(pthread_create(&wild, NULL, &wildpokemonThread, (void *)gameData) < 0){
+                printf("can't create thread\n");
+                exit(EXIT_FAILURE);
+            }
+            while (wpokemon->isLocked){
+                printf ("Capture Mode\n")
+                printf ("Pokemon : %s - Rarity : %d\n", wpokemon->name, wpokemon->type);
+                printf ("\n1. Tangkap\n2. Gunakan lullaby\n3. Keluar\nChoice : ");
+                int z;
+                scanf ("%d",&z);
+                if (pokemon->isLocked==0){
+                    printf("Pokemon kabur\n");
+                    break;
+                }
+                if (z==1){
+                    if (pokeballc > 0){
+                        pokeballc--;
+                        int random = rand()%100;
+                        if (random< wpokemon->capRate + gameData->jumlahLullabyActive * 2){
+                            if (pokecount ==6) printf ("Slot pokemon full\n");
+                            else{
+                                pokecount++;
+                                pokemon[pokecount]->released = 0;
+                                strcpy(pokemon[pokecount]->name,wpokemon->name);
+                                pokemon[pokecount]->type = wpokemon->type;
+                                pokemon[pokecount]->price = wpokemon->price;
+                                pthread_t t;
+                                if(pthread_create(&t, NULL, &pokemonThread, (void *) pokecount) < 0){
+                                    printf("can't create thread\n");
+                                    exit(EXIT_FAILURE);
+                                }
+                                printf ("%s tertangkap!\n",wpokemon->name);
+                                wpokemon->isLocked =0;
+                                state = 0;
+                            }
+                        }
+                        else printf ("tidak tertangkap\n");
+                    }
+                    else printf ("Anda tidak memiliki pokeball\n")
+                }
+                else if(z==2){
+                    if (lullabyPowderc > 0){
+                        lullabyPowderc--;
+                        pthread_t thread;
+                        if(pthread_create(&lullaby, NULL, &lullabyThread,  (void *)gameData) < 0){
+                            printf("can't create thread\n");
+                            exit(EXIT_FAILURE);
+                        }
+                    }
+                    else printf ("Anda tidak memiliki lullaby powder!\n");
+                }
+                else if (z==3){
+                    wpokemon->isLocked = 0;
+                    state = 0;
+                }
+            }
+            wpokemon->isLocked = 0;
         }
     }
 }
 
-void * caripokemon(){
-    // data * temp = (data *) param;
-    while (game->caripokemon == 1){
-        if ((rand()%10)<6){
-            game->caripokemon=0;
-            state = 10;
-            // game->caripokemon =
-            break;
-        }
-    }
-    sleep (10);
+void * cariPokemonThread(void * param){
+	GameData_t * gameData = (GameData_t *) param;
+	while(gameData->cariPokemon){
+		if(rand()%10 <= 5){
+			cariPokemon = 0;
+			state = 3;
+			break;
+		}
+		sleep(10);
+	}
 }
 
-void printshop(){
-    
+void * wildpokemonThread(void * param){
+	GameData_t * gameData = (GameData_t *) param;
+	while (gameData->isRunning && paramisrunning){
+		sleep(20);
+		if(paramisrunnin || !gameData->isRunning){
+			break;
+		}
+		if(gameData->jumlahLullabyActive) continue;
+		int random = rand()%100;
+		if(random < gameData->sharedPokemon->escapeRate){
+			gameData->sharedPokemon->isLocked = 0;
+			break;
+		}
+	}
+	
+}
+
+void * lullabyThread(void * param){
+	GameData_t * gameData = (GameData_t *)param;
+	gameData->jumlahLullabyActive++;
+	sleep(10);
+	gameData->jumlahLullabyActive--;
+}
+
+void * pokemonThread(void * param){
+    int * x = (int *)param;
+	while (*isrunning && !pokemon[x]->released){
+		sleep(10);
+		if(state ==3)continue;
+		pokemon[x]->ap -= 10;
+		if(pokemon[x]->ap == 0){
+			int random = rand()%10;
+			if(random == 0){
+				pokemon[x]->ap = 50;
+			}else{
+				pokemon[x]->released = 1;
+                pokecount--;
+			}
+		}
+	}
+	
 }
